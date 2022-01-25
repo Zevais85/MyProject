9dee4a904645a28918058fc4d3c25c1aa1c7ecc2
Igor Simdyanov <igorsimdyanov@gmail.com>
2022-01-25 12:20:37 +0300

    Исправляем главную страницу

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
-  get 'main_page', to: 'main#index'
+  get 'main_page', to: 'main_page#index'
 end
